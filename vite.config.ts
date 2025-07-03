896842c7976e2786daccd1444837484afebc803a
TrackerJo <nkemme54@gmail.com>
2025-07-03 14:42:30 -0400

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       "@": resolve(__dirname, "./src"),
     },
   },
-  base: '/ShiftMate/',
+  base: '/DecklyPrivacy/',
   build: {
     outDir,
     emptyOutDir: true,
